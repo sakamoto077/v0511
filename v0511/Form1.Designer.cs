99c1f58031d9e2d4d8ff2ef9a806dc15383799cc
sakamoto <130636519+sakamoto077@users.noreply.github.com>
2023-05-11 16:09:35 +0900

    プロパティ

--- v0511/Form1.Designer.cs
+++ v0511/Form1.Designer.cs
@@ -29,23 +29,40 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(32, 36);
+            button1.BackColor = Color.FromArgb(192, 192, 255);
+            button1.Font = new Font("Yu Gothic UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = Color.Red;
+            button1.Location = new Point(150, 150);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(304, 88);
             button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            button1.Text = "あいさつ";
+            button1.TextAlign = ContentAlignment.TopCenter;
+            button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(664, 98);
+            button2.Name = "button2";
+            button2.Size = new Size(90, 23);
+            button2.TabIndex = 1;
+            button2.Text = "復活ボタン２";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(128, 255, 128);
             ClientSize = new Size(800, 450);
+            Controls.Add(button2);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
@@ -55,5 +72,6 @@
         #endregion
 
         private Button button1;
+        private Button button2;
     }
 }
